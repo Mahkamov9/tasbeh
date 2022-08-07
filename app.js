639fff084c8ddac99d587b3ff4637a569a5e2ebb
Mahkamov9 <ahadjonmahkamov111@gmail.com>
2022-08-07 17:11:31 +0500

    end

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const input = document.getElementById("input");
 const incrementBtn = document.getElementById("increment-btn");
 const resetBtn = document.getElementById("reset-btn");
+const colorBtn = document.getElementById("color-btn");
 
 incrementBtn.addEventListener("click" , ()=>{
   input.value++
@@ -8,5 +9,12 @@ incrementBtn.addEventListener("click" , ()=>{
 
 resetBtn.addEventListener("click" , ()=>{
   input.value="0"
+  input.style.backgroundColor= "green"
+
 })
+
+colorBtn.addEventListener("click" , ()=>{
+  input.style.backgroundColor= "red"
+})
+
 
